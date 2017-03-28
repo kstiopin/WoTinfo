ff5400b18690d1f2c59503f845837e8d0bc133b8
Salvation13 <salvation13xs@gmail.com>
2017-03-28 23:28:54 +0300

    fix for adding new accounts to DB

--- ajax/accounts.php
+++ ajax/accounts.php
@@ -14,7 +14,7 @@ if (isset($_POST['account_id'])) {
           '.((count($_POST['angar']) > 0) ? "angar = '".implode(',', $_POST['angar'])."'," : '').'
           updated = NOW() WHERE account_id = ' . $_POST['account_id']) or die(mysql_error());
     } else {
-        $r = mysql_query("INSERT INTO wot_data VALUES (".$_POST['account_id'].", '".$_POST['nickname']."', ".$_POST['battles'].", ".number_format($_POST['winrate'], 2, '.', '').", ".$_POST['wg'].", ".$_POST['wn8'].", NOW(), NULL, NULL, NULL)") or die(mysql_error());
+        $r = mysql_query("INSERT INTO wot_data VALUES (".$_POST['account_id'].", '".$_POST['nickname']."', ".$_POST['battles'].", ".number_format($_POST['winrate'], 2, '.', '').", ".$_POST['wg'].", ".$_POST['wn8'].", NOW(), NULL, NULL)") or die(mysql_error());
     }
 
     die($_POST['account_id']);
