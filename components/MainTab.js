43fb8e5df36a628721e7ca1f8c1a756de572b889
Salvation13 <salvation13xs@gmail.com>
2017-11-17 18:19:12 +0200

    MainTab: better UI for Other users and Auth link

--- components/MainTab.js
+++ components/MainTab.js
@@ -45,73 +45,73 @@ export class MainTab extends Component {
               <div id='update_stats'>
                 <input type='text' id='acc_input' name='acc' ref={ (input) => { this.accInput = input; } }/>
                 <input type='button' className='button red' value='Найти игрока' onClick={ () => getUser(this.accInput.value) } />
-                <a id='authorize' href={ `${apiUrl}auth/login/${applicationId}&redirect_uri=http://wot.kstiopin.in.ua/` }>Авторизироваться для обновления техники в ангаре</a>
               </div>
             </div>
 
-            <div className='b-userblock-wrpr'>
-              <div className='b-user-block b-user-block__sparks'>
-                <div className='b-personal-data'>
-                  <span id="random-link" className={ (activeTab === 'random') ? 'active' : '' } onClick={ () => this.setTab('random') }>Random</span>
-                  <span id="ranked-link" className={ (activeTab === 'ranked') ? 'active' : '' } onClick={ () => this.setTab('ranked') }>Ranked</span>
-                  <table className='t-personal-data'>
-                    <tbody>
-                      <tr>
-                        <th className='t-personal-data_ico t-personal-data_ico__win'>Процент побед</th>
-                        <th className='t-personal-data_ico t-personal-data_ico__fight'>Количество боёв</th>
-                        <th rowSpan='2' className='t-personal-data_pr'>
-                          <div className='t-personal-data_rel'>
-                            <div className='t-personal-data_abs'>
-                              Личный рейтинг
-                              <p className={ `t-personal-data_value t-personal-data_value__pr ${getColor('wg', global_rating)}` } id='personal_rating'>{ global_rating }</p>
-                              WN8
-                              <p className={ `t-personal-data_value t-personal-data_value__pr ${getColor('wn8', wn8)}` } id='personal_wn8'>{ wn8 }</p>
-                            </div>
+            <div className='b-user-block b-user-block__sparks'>
+              <div className='b-personal-data'>
+                <span id="random-link" className={ (activeTab === 'random') ? 'active' : '' } onClick={ () => this.setTab('random') }>Random</span>
+                <span id="ranked-link" className={ (activeTab === 'ranked') ? 'active' : '' } onClick={ () => this.setTab('ranked') }>Ranked</span>
+                <table className='t-personal-data'>
+                  <tbody>
+                    <tr>
+                      <th className='t-personal-data_ico t-personal-data_ico__win'>Процент побед</th>
+                      <th className='t-personal-data_ico t-personal-data_ico__fight'>Количество боёв</th>
+                      <th rowSpan='2' className='t-personal-data_pr'>
+                        <div className='t-personal-data_rel'>
+                          <div className='t-personal-data_abs'>
+                            Личный рейтинг
+                            <p className={ `t-personal-data_value t-personal-data_value__pr ${getColor('wg', global_rating)}` } id='personal_rating'>{ global_rating }</p>
+                            WN8
+                            <p className={ `t-personal-data_value t-personal-data_value__pr ${getColor('wn8', wn8)}` } id='personal_wn8'>{ wn8 }</p>
                           </div>
-                        </th>
-                        <th className='t-personal-data_ico t-personal-data_ico__exp'>Средний опыт за бой</th>
-                        <th className='t-personal-data_ico t-personal-data_ico__dmg'>Средний нанесённый урон за бой</th>
-                      </tr>
-                      <tr>
-                        <td className={ `t-personal-data_value ${getColor('winrate', winrate)}` } id='winrate'>{ `${(winrate).toFixed(2)}%` }</td>
-                        <td className={ `t-personal-data_value ${getColor('battles', battles)}` } id='total_battles'>{ battles }</td>
-                        <td className={ `t-personal-data_value ${getColor('exp', averageExp)}` } id='average_exp'>{ averageExp.toFixed(2) }</td>
-                        <td className={ `t-personal-data_value ${getColor('dmg', averageDmg)}` } id='average_dmg'>{ averageDmg.toFixed(2) }</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+                        </div>
+                      </th>
+                      <th className='t-personal-data_ico t-personal-data_ico__exp'>Средний опыт за бой</th>
+                      <th className='t-personal-data_ico t-personal-data_ico__dmg'>Средний нанесённый урон за бой</th>
+                    </tr>
+                    <tr>
+                      <td className={ `t-personal-data_value ${getColor('winrate', winrate)}` } id='winrate'>{ `${(winrate).toFixed(2)}%` }</td>
+                      <td className={ `t-personal-data_value ${getColor('battles', battles)}` } id='total_battles'>{ battles }</td>
+                      <td className={ `t-personal-data_value ${getColor('exp', averageExp)}` } id='average_exp'>{ averageExp.toFixed(2) }</td>
+                      <td className={ `t-personal-data_value ${getColor('dmg', averageDmg)}` } id='average_dmg'>{ averageDmg.toFixed(2) }</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
-          <div id='other_requests'>
+        </div>
+        <div id='other_requests'>
+          <div className='flex-row top-line'>
             <span onClick={ this.toggleAccounts }>Другие игроки</span>
-            { showAccounts && accounts && <div className='flex-row'>
-              <table id='accounts'>
-                <thead>
-                  <tr>
-                    <th className='pointer' onClick={ () => sortAccounts('nickname') }>Name</th>
-                    <th className='pointer' onClick={ () => sortAccounts('battles') }>Battles</th>
-                    <th className='pointer' onClick={ () => sortAccounts('winrate') }>WR</th>
-                    <th className='pointer' onClick={ () => sortAccounts('wg') }>WG</th>
-                    <th className='pointer' onClick={ () => sortAccounts('wn8') }>WN8</th>
-                    <th>Tanks in angar</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  { accounts.map((account) => (<tr key={ account.account_id }>
-                    <td><span onClick={ () => { this.toggleAccounts(); getUser(account.account_id); } }>{ account.nickname }</span></td>
-                    <td className={ getColor('battles', account.battles) }>{ account.battles }</td>
-                    <td className={ getColor('winrate', account.winrate) }>{ account.winrate }</td>
-                    <td className={ getColor('wg', account.wg) }>{ account.wg }</td>
-                    <td className={ getColor('wn8', account.wn8) }>{ account.wn8 }</td>
-                    <td className="green">{ account.angar ? account.angar.split(',').length : '' }</td>
-                  </tr>)) }
-                </tbody>
-              </table>
-              <ColorScales />
-            </div> }
+            <a href={ `${apiUrl}auth/login/${applicationId}&redirect_uri=http://wot.kstiopin.in.ua/` }>Авторизироваться для обновления техники в ангаре</a>
           </div>
+          { showAccounts && accounts && <div className='flex-row'>
+            <table id='accounts'>
+              <thead>
+              <tr>
+                <th className='pointer' onClick={ () => sortAccounts('nickname') }>Name</th>
+                <th className='pointer' onClick={ () => sortAccounts('battles') }>Battles</th>
+                <th className='pointer' onClick={ () => sortAccounts('winrate') }>WR</th>
+                <th className='pointer' onClick={ () => sortAccounts('wg') }>WG</th>
+                <th className='pointer' onClick={ () => sortAccounts('wn8') }>WN8</th>
+                <th>Tanks in angar</th>
+              </tr>
+              </thead>
+              <tbody>
+              { accounts.map((account) => (<tr key={ account.account_id }>
+                <td><span onClick={ () => { this.toggleAccounts(); getUser(account.account_id); } }>{ account.nickname }</span></td>
+                <td className={ getColor('battles', account.battles) }>{ account.battles }</td>
+                <td className={ getColor('winrate', account.winrate) }>{ account.winrate }</td>
+                <td className={ getColor('wg', account.wg) }>{ account.wg }</td>
+                <td className={ getColor('wn8', account.wn8) }>{ account.wn8 }</td>
+                <td className="green">{ account.angar ? account.angar.split(',').length : '' }</td>
+              </tr>)) }
+              </tbody>
+            </table>
+            <ColorScales />
+          </div> }
         </div>
         <div className='clear'></div>
       </div>);
